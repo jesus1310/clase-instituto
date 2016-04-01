1fc416cb5c864983547b5b8b19864a9c3bc51d9f
Jesus <jfr1013@gmail.com>
2016-04-01 13:13:38 +0200

    Añadido constructor

--- Alumno.java
+++ Alumno.java
@@ -6,11 +6,21 @@
  */
 public class Alumno
 {
+    private String nombre;
+    private int edad;
+    private int numeroInicial = 1;
+    private int numeroLista;
+    private ArrayListInt notas;
+    
     /**
      * Constructor for objects of class Alumno
      */
-    public Alumno()
+    public Alumno(String nombre,int edad)
     {
-        
-    }
+        this.nombre = nombre;
+        this.edad = edad;
+        numeroLista = numeroInicial;
+        numeroInicial++;
+        notas = new ArrayListInt();
+    }    
 }
