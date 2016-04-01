a781acb7251164018966e5d060912586dc6c1f03
Jesus <jfr1013@gmail.com>
2016-04-01 13:03:37 +0200

    Añadida clase Alumno

---
+++ Alumno.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class Alumno here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Alumno
+{
+    /**
+     * Constructor for objects of class Alumno
+     */
+    public Alumno()
+    {
+        
+    }
+}
